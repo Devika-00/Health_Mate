8758152d5486b3d1af90fbaa92fe33aa3ace3a04
Devika P S <devu1011depu@gmail.com>
2024-06-10 21:38:42 +0530

    build

--- backend/dist/utils/userEmail.js
+++ backend/dist/utils/userEmail.js
@@ -87,7 +87,7 @@ const forgotPasswordEmail = (name, verificationCode) => {
                                 <p>Dear ${name},</p>
                                 <p>We have received a request to reset your password. To reset your password, click the button below:</p>
                                 <p style="text-align: center;">
-                                    <a href="http://localhost:5173/user/reset_password/${verificationCode}" style="display: inline-block; padding: 12px 24px; background-color: #007bff; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">Reset Password</a>
+                                    <a href="https://healthmate.dpsdev.site/user/reset_password/${verificationCode}" style="display: inline-block; padding: 12px 24px; background-color: #007bff; color: #ffffff; text-decoration: none; border-radius: 5px; font-weight: bold;">Reset Password</a>
                                 </p>
                                 <p>If you didn't request a password reset, you can ignore this email. Your password will remain unchanged.</p>
                                 <p>Thank you for using our service!</p>
---
+++ frontend/dist/assets/toaster-B89phBrG.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./index-CPlNsI3o.js";const o=(s,r="success")=>{switch(r){case"success":c.success(s);break;case"error":c.error(s);break;default:c.success(s);break}};export{o as s};
