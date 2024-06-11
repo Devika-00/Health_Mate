a4043e54353fb3d1b3d939da64a406ee7298da43
Devika P S <devu1011depu@gmail.com>
2024-06-11 23:50:31 +0530

    testing

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
 const expressConfig_1 = __importDefault(require("./frameworks/webserver/expressConfig"));
 const server_1 = __importDefault(require("./frameworks/webserver/server"));
 const connection_1 = __importDefault(require("./frameworks/database/mongodb/connection"));
@@ -23,16 +24,31 @@ const io = new socket_io_1.Server(httpServer, {
         credentials: true,
     },
 });
+// Serve static files from the React app
 app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
+// Set up Content Security Policy (CSP)
+app.use(helmet_1.default.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", "https://api.cloudinary.com"],
+        imgSrc: ["'self'", "data:", "https://res.cloudinary.com"], // Allow loading images from Cloudinary
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+    },
+}));
 (0, socket_1.default)(io);
 (0, expressConfig_1.default)(app);
 (0, connection_1.default)();
 (0, routes_1.default)(app);
 (0, server_1.default)(httpServer);
+// Catch-all route to serve index.html
 app.get("*", (req, res) => {
     res.sendFile(path_1.default.join(__dirname, "../../frontend/dist/index.html"));
 });
+// Error handling middleware
 app.use(errorhandler_middleware_1.default);
+// Catch-all route for 404 errors
 app.all("*", (req, res, next) => {
     next(new customError_1.default(`Not found : ${req.url}`, 404));
 });
+exports.default = app;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
-import express, {Application,NextFunction,Request,Response} from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import helmet from "helmet";
 import expressConfig from "./frameworks/webserver/expressConfig";
 import startServer from "./frameworks/webserver/server";
 import connectDB from "./frameworks/database/mongodb/connection";
@@ -10,8 +11,7 @@ import { Server } from "socket.io";
 import socketConfig from "./frameworks/webserver/webSocket/socket";
 import path from "path";
 
-
-const app:Application = express();
+const app: Application = express();
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
@@ -22,8 +22,20 @@ const io = new Server(httpServer, {
   },
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+// Set up Content Security Policy (CSP)
 app.use(
-  express.static(path.join(__dirname, "../../frontend/dist"))
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      connectSrc: ["'self'", "https://api.cloudinary.com"],
+      imgSrc: ["'self'", "data:", "https://res.cloudinary.com"], // Allow loading images from Cloudinary
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+    },
+  })
 );
 
 socketConfig(io);
@@ -32,15 +44,17 @@ connectDB();
 routes(app);
 startServer(httpServer);
 
-
+// Catch-all route to serve index.html
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(
-    path.join(__dirname, "../../frontend/dist/index.html")
-  );
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
-
+// Error handling middleware
 app.use(errorHandlingMiddleware);
-app.all("*",(req, res, next: NextFunction)=>{
-    next(new CustomError(`Not found : ${req.url}`, 404));
+
+// Catch-all route for 404 errors
+app.all("*", (req, res, next: NextFunction) => {
+  next(new CustomError(`Not found : ${req.url}`, 404));
 });
+
+export default app;
